fd7f4f23b0bbbc5a16bf73a7d0d330100503b4a0
pocj8ur4in <pocj8ur4in@gmail.com>
2024-06-24 16:41:41 +0900

    🔧 BaseEntity 추가

---
+++ src/main/java/hana/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package hana.common.entity;
+
+import hana.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseEntity extends BaseTimeEntity {
+    @Column(name = "deleted_yn", nullable = false)
+    private boolean deletedYn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by")
+    private Member createdBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "updated_by")
+    private Member updatedBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "deleted_by")
+    private Member deletedBy;
+}
