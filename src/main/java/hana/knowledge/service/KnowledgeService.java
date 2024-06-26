c8a213012b2b88759f903022a3743bb805184ad2
jungjaegun <jregun@naver.com>
2024-06-26 14:05:57 +0900

    ✨금융상식 목록 조회 구현

--- src/main/java/hana/knowledge/service/KnowledgeService.java
+++ src/main/java/hana/knowledge/service/KnowledgeService.java
@@ -1,8 +1,35 @@
 package hana.knowledge.service;
 
 import hana.common.annotation.TypeInfo;
+import hana.knowledge.domain.KnowledgeRepository;
+import hana.knowledge.dto.KnowledgesReadResDto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @TypeInfo(name = "KnowledgeService", description = "금융상식 서비스")
 @Service
-public class KnowledgeService {}
+public class KnowledgeService {
+    private final KnowledgeRepository knowledgeRepository;
+
+    public KnowledgesReadResDto getKnowledges() {
+        List<KnowledgesReadResDto.Data> data =
+                knowledgeRepository.findAll().stream()
+                        .map(
+                                knowledge ->
+                                        KnowledgesReadResDto.Data.builder()
+                                                .knowledgeIdx(knowledge.getKnowledgeIdx())
+                                                .knowledgeTitle(knowledge.getKnowledgeTitle())
+                                                .knowledgeSummary(knowledge.getKnowledgeContent())
+                                                .knowledgeImage(knowledge.getKnowledgeImage())
+                                                .build())
+                        .collect(Collectors.toCollection(ArrayList::new));
+
+        return new KnowledgesReadResDto(data);
+    }
+
+    public KnowledgeService(KnowledgeRepository knowledgeRepository) {
+        this.knowledgeRepository = knowledgeRepository;
+    }
+}
