9a66d6ea1bb58aa8cf4b31bac6a8387d9968252a
pocj8ur4in <pocj8ur4in@gmail.com>
2024-06-26 11:42:12 +0900

    ♻️ 회원 간편로그인 로직 수정

--- src/main/java/hana/member/service/StudentService.java
+++ src/main/java/hana/member/service/StudentService.java
@@ -1,8 +1,25 @@
 package hana.member.service;
 
+import hana.common.annotation.MethodInfo;
 import hana.common.annotation.TypeInfo;
+import hana.member.domain.Member;
+import hana.member.domain.Student;
+import hana.member.domain.StudentRepository;
+import lombok.Builder;
 import org.springframework.stereotype.Service;
 
 @TypeInfo(name = "StudentService", description = "학생 서비스")
 @Service
-public class StudentService {}
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @MethodInfo(name = "findStudentByMemberIdx", description = "회원 번호로 학생 정보를 조회합니다.")
+    public Student findStudentByMemberIdx(Member member) {
+        return studentRepository.findByMember(member);
+    }
+
+    @Builder
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+}
