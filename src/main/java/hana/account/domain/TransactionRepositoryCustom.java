3e09eac6aa8b306e6a33c427c7c010a4be1940b3
Hyeyoung Moon <76941500+mummhy0811@users.noreply.github.com>
2024-06-27 14:15:27 +0900

    fix: 거래 내역 조회 날짜로 그룹화 (#30)
    :recycle: 거래 내역 조회 날짜로 그룹화

--- src/main/java/hana/account/domain/TransactionRepositoryCustom.java
+++ src/main/java/hana/account/domain/TransactionRepositoryCustom.java
@@ -1,10 +1,11 @@
 package hana.account.domain;
 
 import hana.account.dto.DeptAccountTransactionResDto;
+import hana.account.dto.TransactionsByDateResDto;
 import java.util.List;
 
 public interface TransactionRepositoryCustom {
-    List<DeptAccountTransactionResDto> getTransactionsByDate(
+    List<TransactionsByDateResDto> getTransactionsByDate(
             Long accountIdx, String startDate, String endDate, String type, String sort, Long page);
 
     List<DeptAccountTransactionResDto> getTransactionsByStory(Long storyIdx);
