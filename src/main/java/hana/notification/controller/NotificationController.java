66edd057916cd5a06c561991793e83eb8ac886aa
hejin8307 <hejin8307@naver.com>
2024-07-01 17:30:06 +0900

    deploy: firebase 배포 오류 수정 중

--- src/main/java/hana/notification/controller/NotificationController.java
+++ src/main/java/hana/notification/controller/NotificationController.java
@@ -21,19 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
     private final FCMService fcmService;
 
-    @PostMapping("/notificationTest")
-    public ResponseEntity<NotificationTestDto<Object>> pushMessage(
-            @RequestBody @Validated FcmSendReqDto fcmSendReqDto) throws IOException {
-        int result = fcmService.sendMessageTo(fcmSendReqDto);
-
-        NotificationTestDto<Object> test =
-                NotificationTestDto.builder()
-                        .result(result)
-                        .resultCode(200)
-                        .resultMessage("알림 성공했습니다.")
-                        .build();
-        return new ResponseEntity<>(test, HttpStatus.OK);
-    }
+//    @PostMapping("/notificationTest")
+//    public ResponseEntity<NotificationTestDto<Object>> pushMessage(
+//            @RequestBody @Validated FcmSendReqDto fcmSendReqDto) throws IOException {
+//        int result = fcmService.sendMessageTo(fcmSendReqDto);
+//
+//        NotificationTestDto<Object> test =
+//                NotificationTestDto.builder()
+//                        .result(result)
+//                        .resultCode(200)
+//                        .resultMessage("알림 성공했습니다.")
+//                        .build();
+//        return new ResponseEntity<>(test, HttpStatus.OK);
+//    }
 
     public NotificationController(FCMService fcmService) {
         this.fcmService = fcmService;
