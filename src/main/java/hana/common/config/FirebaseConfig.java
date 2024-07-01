5a997a15d067cde91ed3daca4575cfc4f959ed24
hejin8307 <hejin8307@naver.com>
2024-07-01 17:31:42 +0900

    deploy: firebase 배포 오류 수정 중

--- src/main/java/hana/common/config/FirebaseConfig.java
+++ src/main/java/hana/common/config/FirebaseConfig.java
@@ -1,23 +1,23 @@
 package hana.common.config;
 
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import com.google.firebase.messaging.FirebaseMessaging;
-//import com.google.gson.Gson;
-//import com.google.gson.JsonObject;
-//import com.google.gson.JsonParser;
-//import java.io.ByteArrayInputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.nio.charset.StandardCharsets;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.ClassPathResource;
+// import com.google.auth.oauth2.GoogleCredentials;
+// import com.google.firebase.FirebaseApp;
+// import com.google.firebase.FirebaseOptions;
+// import com.google.firebase.messaging.FirebaseMessaging;
+// import com.google.gson.Gson;
+// import com.google.gson.JsonObject;
+// import com.google.gson.JsonParser;
+// import java.io.ByteArrayInputStream;
+// import java.io.IOException;
+// import java.io.InputStream;
+// import java.nio.charset.StandardCharsets;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.core.io.ClassPathResource;
 //
-//@Configuration
-//public class FirebaseConfig {
+// @Configuration
+// public class FirebaseConfig {
 //    @Value("${fcm.google_application_credentials}")
 //    private String GOOGLE_APPLICATION_CREDENTIALS;
 //
@@ -61,4 +61,4 @@ package hana.common.config;
 //    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
 //        return FirebaseMessaging.getInstance(firebaseApp);
 //    }
-//}
+// }
