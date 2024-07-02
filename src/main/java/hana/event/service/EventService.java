793d3782511f5e6d0612fab36984f8bd52eb896f
pocj8ur4in <pocj8ur4in@gmail.com>
2024-07-03 00:10:03 +0900

    ✨ 이벤트 스케줄링 구현

--- src/main/java/hana/event/service/EventService.java
+++ src/main/java/hana/event/service/EventService.java
@@ -2,8 +2,8 @@ package hana.event.service;
 
 import hana.common.annotation.MethodInfo;
 import hana.common.annotation.TypeInfo;
-import hana.event.domain.Event;
-import hana.event.domain.EventRepository;
+import hana.event.domain.*;
+import hana.member.domain.MemberToken;
 import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EventService {
     private final EventRepository eventRepository;
+    private final ScheduledEventRepository scheduledEventRepository;
 
     @MethodInfo(name = "countEvent", description = "이벤트 개수를 조회합니다.")
     public Long countEvent() {
@@ -35,7 +36,85 @@ public class EventService {
         }
     }
 
-    public EventService(EventRepository eventRepository) {
+    @MethodInfo(name = "readEventPrizes", description = "이벤트 상품들을 조회합니다.")
+    public List<EventPrize> readEventPrizes(Long eventIdx) {
+        return eventPrizeRepository.findAllByEventIdx(eventIdx);
+    }
+
+    @MethodInfo(name = "createEventWinners", description = "이벤트 당첨자들을 생성합니다.")
+    public void createEventWinners(List<EventWinner> eventWinners) {
+        eventWinnerRepository.saveAll(eventWinners);
+    }
+
+    @MethodInfo(name = "createEventArrival", description = "이벤트 신청자를 생성합니다.")
+    public void createEventArrival(EventArrival eventArrival) {
+        eventArrivalRepository.save(eventArrival);
+    }
+
+    @MethodInfo(name = "scheduleEvent", description = "이벤트를 예약합니다.")
+    public ScheduledEvent scheduleEvent(ScheduledEvent scheduledEvent) {
+        return scheduledEventRepository.save(scheduledEvent);
+    }
+
+    @MethodInfo(name = "drawEventWinners", description = "이벤트 당첨자를 추첨합니다.")
+    public List<EventWinner> drawEventWinners(
+            List<EventPrize> eventPrizes, List<EventArrival> eventArrivals) {
+        int totalPrizeCount = 0;
+        for (EventPrize eventPrize : eventPrizes) {
+            totalPrizeCount += eventPrize.getEventPrizeLimit();
+        }
+        ArrayList<EventWinner> eventWinners = new ArrayList<>();
+
+        while (totalPrizeCount > 0) {
+            int randomIndex = random.nextInt(eventArrivals.size());
+
+            EventArrival eventArrival = eventArrivals.get(randomIndex);
+            if (eventArrival.getIsWinner()) {
+                continue;
+            }
+            eventArrival.setIsWinner(true);
+
+            EventPrize eventPrize = eventPrizes.get(random.nextInt(eventPrizes.size()));
+
+            if (eventPrize.getEventPrizeLimit() == 0) {
+                eventArrival.setIsWinner(false);
+                continue;
+            }
+            eventPrize.setEventPrizeLimit(eventPrize.getEventPrizeLimit() - 1);
+
+            eventWinners.add(
+                    EventWinner.builder()
+                            .student(eventArrival.getStudent())
+                            .eventPrize(eventPrize)
+                            .build());
+
+            totalPrizeCount--;
+        }
+
+        return eventWinners;
+    }
+
+    @MethodInfo(name = "updateEventPush", description = "이벤트 푸시를 업데이트합니다.")
+    public void updateEventPush(Long eventIdx, MemberToken memberToken) {
+        eventTokenRepository.save(
+                EventToken.builder()
+                        .memberIdx(memberToken.getMemberIdx())
+                        .studentIdx(memberToken.getStudentIdx())
+                        .eventIdx(eventIdx)
+                        .deptIdx(memberToken.getDeptIdx())
+                        .fcmToken(memberToken.getFcmToken())
+                        .ttl(memberToken.getTtl())
+                        .build());
+    }
+
+    @MethodInfo(name = "findAllEventTokensByDeptIdx", description = "학과 식별자로 이벤트 토큰 목록을 조회합니다.")
+    public List<EventToken> findAllEventTokensByDeptIdx(Long deptIdx) {
+        return eventTokenRepository.findAllByDeptIdx(deptIdx);
+    }
+
+    public EventService(
+            EventRepository eventRepository,
+            EventTokenRepository eventTokenRepository) {
         this.eventRepository = eventRepository;
     }
 }
---
+++ src/main/java/hana/event/task/ScheduledEventTasks.java
@@ -0,0 +1,196 @@
+package hana.event.task;
+
+import hana.common.annotation.MethodInfo;
+import hana.common.annotation.TypeInfo;
+import hana.common.dto.notification.FcmSendReqDto;
+import hana.common.service.FCMService;
+import hana.event.domain.*;
+import hana.event.service.EventService;
+import hana.member.domain.MemberToken;
+import hana.member.service.MemberTokenService;
+import java.io.IOException;
+import java.util.List;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@TypeInfo(name = "ScheduledEventTasks", description = "스케줄 이벤트 태스크")
+@Component
+public class ScheduledEventTasks {
+    private final EventService eventService;
+    private final MemberTokenService memberTokenService;
+    private final FCMService fcmService;
+
+    @MethodInfo(name = "executeScheduledEventTasks", description = "스케줄된 이벤트 태스크를 실행합니다.")
+    @Scheduled(cron = "0 * * * * ?")
+    public void scheduledEventTasks() throws IOException {
+        List<ScheduledEvent> scheduledEvents = eventService.readScheduledEvents();
+
+        for (ScheduledEvent scheduledEvent : scheduledEvents) {
+            List<MemberToken> memberTokens =
+                    memberTokenService.findAllByDeptIdx(scheduledEvent.getDeptIdx());
+            List<EventToken> eventTokens =
+                    eventService.findAllEventTokensByDeptIdx(scheduledEvent.getEventIdx());
+
+            if (scheduledEvent.getScheduledEventType().equals(ScheduledEventEnumType.신청_시작)) {
+                for (MemberToken memberToken : memberTokens) {
+                    fcmService.sendMessageTo(
+                            FcmSendReqDto.builder()
+                                    .token(memberToken.getFcmToken())
+                                    .title("더영하나")
+                                    .body(
+                                            "["
+                                                    + scheduledEvent.getEventTitle()
+                                                    + "] 이벤트 신청이 시작되었습니다. "
+                                                    + scheduledEvent.getEventEndDatetime().getYear()
+                                                    + "년 "
+                                                    + scheduledEvent
+                                                            .getEventEndDatetime()
+                                                            .getMonthValue()
+                                                    + "월 "
+                                                    + scheduledEvent
+                                                            .getEventEndDatetime()
+                                                            .getDayOfMonth()
+                                                    + "일 "
+                                                    + scheduledEvent.getEventEndDatetime().getHour()
+                                                    + "시 "
+                                                    + scheduledEvent
+                                                            .getEventEndDatetime()
+                                                            .getMinute()
+                                                    + "분 "
+                                                    + scheduledEvent
+                                                            .getEventEndDatetime()
+                                                            .getSecond()
+                                                    + "초까지 신청 가능합니다.")
+                                    .build());
+                }
+            } else if (scheduledEvent
+                    .getScheduledEventType()
+                    .equals(ScheduledEventEnumType.신청_마감)) {
+                for (MemberToken memberToken : memberTokens) {
+                    fcmService.sendMessageTo(
+                            FcmSendReqDto.builder()
+                                    .token(memberToken.getFcmToken())
+                                    .title("더영하나")
+                                    .body(
+                                            "["
+                                                    + scheduledEvent.getEventTitle()
+                                                    + "] 이벤트 신청이 마감되었습니다.")
+                                    .build());
+                }
+            } else if (scheduledEvent
+                    .getScheduledEventType()
+                    .equals(ScheduledEventEnumType.입금_시작)) {
+                for (EventToken eventToken : eventTokens) {
+                    fcmService.sendMessageTo(
+                            FcmSendReqDto.builder()
+                                    .token(eventToken.getFcmToken())
+                                    .title("더영하나")
+                                    .body(
+                                            "["
+                                                    + scheduledEvent.getEventTitle()
+                                                    + "] 이벤트 입금이 시작되었습니다. "
+                                                    + scheduledEvent
+                                                            .getEventFeeEndDatetime()
+                                                            .getYear()
+                                                    + "년 "
+                                                    + scheduledEvent
+                                                            .getEventFeeEndDatetime()
+                                                            .getMonthValue()
+                                                    + "월 "
+                                                    + scheduledEvent
+                                                            .getEventFeeEndDatetime()
+                                                            .getDayOfMonth()
+                                                    + "일 "
+                                                    + scheduledEvent
+                                                            .getEventFeeEndDatetime()
+                                                            .getHour()
+                                                    + "시 "
+                                                    + scheduledEvent
+                                                            .getEventFeeEndDatetime()
+                                                            .getMinute()
+                                                    + "분 "
+                                                    + scheduledEvent
+                                                            .getEventFeeEndDatetime()
+                                                            .getSecond()
+                                                    + "초까지"
+                                                    + scheduledEvent.getEventFee()
+                                                    + "원을 입금해주세요.")
+                                    .build());
+                }
+            } else if (scheduledEvent
+                    .getScheduledEventType()
+                    .equals(ScheduledEventEnumType.입금_마감)) {
+                for (EventToken eventToken : eventTokens) {
+                    fcmService.sendMessageTo(
+                            FcmSendReqDto.builder()
+                                    .token(eventToken.getFcmToken())
+                                    .title("더영하나")
+                                    .body(
+                                            "["
+                                                    + scheduledEvent.getEventTitle()
+                                                    + "] 이벤트 입금이 마감되었습니다.")
+                                    .build());
+                }
+            } else if (scheduledEvent
+                    .getScheduledEventType()
+                    .equals(ScheduledEventEnumType.응모_시작)) {
+
+                eventService.createEventWinners(
+                        eventService.drawEventWinners(
+                                eventService.readEventPrizes(scheduledEvent.getEventIdx()),
+                                eventService.readEventArrivals(scheduledEvent.getEventIdx())));
+
+                for (MemberToken memberToken : memberTokens) {
+                    fcmService.sendMessageTo(
+                            FcmSendReqDto.builder()
+                                    .token(memberToken.getFcmToken())
+                                    .title("더영하나")
+                                    .body(
+                                            "["
+                                                    + scheduledEvent.getEventTitle()
+                                                    + "] 이벤트 응모가 마감되었습니다. 결과 페이지에서 응모 결과를 확인해보세요.")
+                                    .build());
+                }
+            } else if (scheduledEvent
+                    .getScheduledEventType()
+                    .equals(ScheduledEventEnumType.선착_시작)) {
+                for (MemberToken memberToken : memberTokens) {
+                    fcmService.sendMessageTo(
+                            FcmSendReqDto.builder()
+                                    .token(memberToken.getFcmToken())
+                                    .title("더영하나")
+                                    .body(
+                                            "["
+                                                    + scheduledEvent.getEventTitle()
+                                                    + "] 이벤트 선착순 신청이 시작되었습니다. "
+                                                    + scheduledEvent.getEventDatetime().getYear()
+                                                    + "년 "
+                                                    + scheduledEvent
+                                                            .getEventDatetime()
+                                                            .getMonthValue()
+                                                    + "월 "
+                                                    + scheduledEvent
+                                                            .getEventDatetime()
+                                                            .getDayOfMonth()
+                                                    + "일 "
+                                                    + scheduledEvent.getEventDatetime().getHour()
+                                                    + "시 "
+                                                    + scheduledEvent.getEventDatetime().getMinute()
+                                                    + "분 "
+                                                    + scheduledEvent.getEventDatetime().getSecond()
+                                                    + "초까지 신청 가능합니다.")
+                                    .build());
+                }
+            }
+        }
+    }
+
+    public ScheduledEventTasks(
+            EventService eventService,
+            MemberTokenService memberTokenService,
+            FCMService fcmService) {
+        this.eventService = eventService;
+        this.memberTokenService = memberTokenService;
+        this.fcmService = fcmService;
+    }
+}
