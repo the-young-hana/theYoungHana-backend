843415555aff30c29682b6a7897045bc0cce1cd6
pocj8ur4in <pocj8ur4in@gmail.com>
2024-07-02 23:33:38 +0900

    :recycle: 회원 간편로그인 API 수정 (FCM 토큰 추가)

--- src/main/java/hana/member/controller/MemberController.java
+++ src/main/java/hana/member/controller/MemberController.java
@@ -2,12 +2,16 @@ package hana.member.controller;
 
 import hana.common.annotation.MethodInfo;
 import hana.common.annotation.TypeInfo;
+import hana.common.dto.notification.FcmSendReqDto;
 import hana.common.exception.BaseExceptionResponse;
+import hana.common.service.FCMService;
 import hana.common.vo.JwtToken;
+import hana.member.domain.MemberToken;
 import hana.member.domain.Student;
 import hana.member.dto.MemberLoginReqDto;
 import hana.member.dto.MemberLoginResDto;
 import hana.member.service.MemberService;
+import hana.member.service.MemberTokenService;
 import hana.member.service.StudentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,6 +19,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.io.IOException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +30,8 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
     private final StudentService studentService;
+    private final MemberTokenService memberTokenService;
+    private final FCMService fcmService;
 
     @MethodInfo(name = "login", description = "회원 간편 로그인을 실행합니다.")
     @PostMapping("/member/login")
@@ -79,11 +86,33 @@ public class MemberController {
                                                                 BaseExceptionResponse.class)))
             })
     public ResponseEntity<MemberLoginResDto> login(
-            @Valid @RequestBody MemberLoginReqDto memberLoginReqDto) {
+            @Valid @RequestBody MemberLoginReqDto memberLoginReqDto) throws IOException {
         JwtToken jwtToken = memberService.login(memberLoginReqDto.getPassword());
         Student student =
                 studentService.findStudentByMemberIdx(
                         memberService.findByMemberPw(memberLoginReqDto.getPassword()));
+
+        memberTokenService.save(
+                MemberToken.builder()
+                        .memberIdx(student.getMember().getMemberIdx())
+                        .studentIdx(student.getStudentIdx())
+                        .deptIdx(student.getDept().getDeptIdx())
+                        .fcmToken(memberLoginReqDto.getFcmToken())
+                        .accessToken(jwtToken.getAccessToken())
+                        .refreshToken(jwtToken.getRefreshToken())
+                        .ttl(60L * 60L * 24L * 60L)
+                        .build());
+
+        fcmService.sendMessageTo(
+                FcmSendReqDto.builder()
+                        .token(memberLoginReqDto.getFcmToken())
+                        .title("더영하나")
+                        .body(
+                                student.getStudentName()
+                                        + "님, 환영합니다. "
+                                        + student.getDept().getDeptName()
+                                        + "에서 진행 중인 이벤트를 확인하세요.")
+                        .build());
 
         return ResponseEntity.ok(
                 MemberLoginResDto.builder()
@@ -96,8 +125,14 @@ public class MemberController {
                         .build());
     }
 
-    public MemberController(MemberService memberService, StudentService studentService) {
+    public MemberController(
+            MemberService memberService,
+            StudentService studentService,
+            MemberTokenService memberTokenService,
+            FCMService fcmService) {
         this.memberService = memberService;
         this.studentService = studentService;
+        this.memberTokenService = memberTokenService;
+        this.fcmService = fcmService;
     }
 }
