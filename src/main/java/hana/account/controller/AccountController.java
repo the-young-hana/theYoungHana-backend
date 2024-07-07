312ba39b724c1d44e858c399cdb8168fa3345a8f
Hyeyoung Moon <76941500+mummhy0811@users.noreply.github.com>
2024-07-07 22:14:39 +0900

    feat: 계좌 비밀번호 확인 API 생성 (#130)
    :sparkles: 계좌 비밀번호 확인 API 생성

--- src/main/java/hana/account/controller/AccountController.java
+++ src/main/java/hana/account/controller/AccountController.java
@@ -1,5 +1,7 @@
 package hana.account.controller;
 
+import hana.account.dto.AccountPwCheckReqDto;
+import hana.account.dto.AccountPwCheckResDto;
 import hana.account.dto.AccountReadResDto;
 import hana.account.service.AccountService;
 import hana.common.annotation.MethodInfo;
@@ -12,9 +14,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @TypeInfo(name = "AccountController", description = "계좌 컨트롤러")
 @RestController
@@ -24,7 +24,7 @@ public class AccountController {
     private final AccountService accountService;
     private final JwtUtils jwtUtils;
 
-    @MethodInfo(name = "readDeptInfo", description = "내 계좌 목록을 조회합니다.")
+    @MethodInfo(name = "readAccountInfo", description = "내 계좌 목록을 조회합니다.")
     @GetMapping("/accounts")
     @Operation(
             summary = "내 계좌 목록 조회",
@@ -68,6 +68,66 @@ public class AccountController {
             })
     public ResponseEntity<AccountReadResDto> readAccounts() {
         return ResponseEntity.ok(accountService.getMyAccounts(jwtUtils.getMember().getMemberIdx()));
+    }
+
+    @MethodInfo(name = "readDeptInfo", description = "계좌 비밀번호를 확인합니다.")
+    @PostMapping("/accounts")
+    @Operation(
+            summary = "계좌 비밀번호 확인",
+            description = "계좌 비밀번호를 확인합니다.",
+            method = "POST",
+            requestBody =
+                    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                            description = "비밀번호 확인",
+                            required = true,
+                            content =
+                                    @Content(
+                                            schema =
+                                                    @Schema(
+                                                            implementation =
+                                                                    AccountPwCheckReqDto.class))),
+            responses = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "계좌 비밀번호 확인 성공",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                AccountPwCheckResDto.class))),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "계좌 비밀번호 확인 실패",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class))),
+                @ApiResponse(
+                        responseCode = "403",
+                        description = "접근 권한 없음",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "서버 오류",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class)))
+            })
+    public ResponseEntity<AccountPwCheckResDto> checkAccountPw(
+            @RequestBody AccountPwCheckReqDto accountPwCheckReqDto) {
+        return ResponseEntity.ok(
+                accountService.checkAccountPw(jwtUtils.getMember(), accountPwCheckReqDto));
     }
 
     public AccountController(AccountService accountService, JwtUtils jwtUtils) {
